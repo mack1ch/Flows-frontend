50311886856eb6b50bc054dbf4876246f79fa79e
Дмитрий <mack1ch@DmitriyStepanov.local>
2023-12-29 14:34:15 +0500

    update main layout

--- src/app/(routing)/page.tsx
+++ src/app/(routing)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <p style={{ color: '#222' }}>Hello</p>
+            <p style={{ color: '#222',  }}>Hello</p>
         </>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.scss';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Inverse Заявки',
@@ -12,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ru">
-            <body className={inter.className}>{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
