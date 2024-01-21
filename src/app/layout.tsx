1f95e1080bf4481f8d155743ef7f0f2f437e74bc
unknown <dimastepanov.1995@mail.ru>
2024-01-21 08:59:29 +0300

    fixed: bug with server render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.scss';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 export const metadata: Metadata = {
     title: 'Inverse Заявки',
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ru">
-            <body>{children}</body>
+            <body>
+                <AntdRegistry>{children}</AntdRegistry>
+            </body>
         </html>
     );
 }
