032af2a5797c0533a1424b1ca96146c4e5e7ed2a
unknown <dimastepanov.1995@mail.ru>
2024-01-23 09:47:35 +0300

    fixed: bug with auth-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="ru">
             <body>
-                <AntdRegistry>{children}</AntdRegistry>
+                {children}
             </body>
         </html>
     );
