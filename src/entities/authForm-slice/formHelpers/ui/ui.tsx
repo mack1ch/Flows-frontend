6c3906807b29d3ca0462d4ab2c6d55722ee0a337
mack1ch <dimastepanov.1995@mail.ru>
2024-04-10 22:13:23 +0500

    removed: remember password

--- src/entities/authForm-slice/formHelpers/ui/ui.tsx
+++ src/entities/authForm-slice/formHelpers/ui/ui.tsx
@@ -1,21 +1,25 @@
-import { Checkbox, ConfigProvider, ThemeConfig } from 'antd'
-import styles from './ui.module.scss'
+import { Checkbox, ConfigProvider, ThemeConfig } from 'antd';
+import styles from './ui.module.scss';
 
 export const FormHelpers = () => {
-    return (<>
-    <ConfigProvider theme={fotmHelpersTheme}>
-        <div className={styles.layout}>
-            <Checkbox><p className={styles.helperText}>Запомнить меня</p></Checkbox >
-            <p className={styles.helperText}>Забыли пароль?</p>
-        </div>
-        </ConfigProvider>
-    </>)
-}
+    return (
+        <>
+            <ConfigProvider theme={fotmHelpersTheme}>
+                <div className={styles.layout}>
+                    <Checkbox>
+                        <p className={styles.helperText}>Запомнить меня</p>
+                    </Checkbox>
+                    {/* <p className={styles.helperText}>Забыли пароль?</p> */}
+                </div>
+            </ConfigProvider>
+        </>
+    );
+};
 const fotmHelpersTheme: ThemeConfig = {
-    components:{
-        Checkbox:{
+    components: {
+        Checkbox: {
             colorPrimary: '#73AE62',
             colorPrimaryHover: '#538A1B',
-        }
-    }
-}
+        },
+    },
+};
