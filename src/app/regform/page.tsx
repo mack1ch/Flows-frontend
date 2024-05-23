1deb8837f8d06545938d04648b86812ad2b7daa0
mack1ch <dimastepanov.1995@mail.ru>
2024-05-23 14:22:27 +0500

    redesigned: news-slice

--- src/app/regform/page.tsx
+++ src/app/regform/page.tsx
@@ -1,5 +1,9 @@
-import { RegForm } from "@/features/regForm-slice/regForm";
+import { RegForm } from '@/features/regForm-slice/regForm';
 
-export default function RegFormScreen() {
-    return <><RegForm/></>;
+export default function RegFormScreen({ params }: { params: { id: number } }) {
+    return (
+        <>
+            <RegForm id={params.id} />
+        </>
+    );
 }
