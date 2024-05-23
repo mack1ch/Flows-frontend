9483478e39f906a2fef9007f10d2c8ca51609650
mack1ch <dimastepanov.1995@mail.ru>
2024-05-23 14:28:05 +0500

    redeploy

--- src/app/regform/page.tsx
+++ src/app/regform/page.tsx
@@ -3,6 +3,7 @@ import { RegForm } from '@/features/regForm-slice/regForm';
 export default function RegFormScreen({ params }: { params: { id: number } }) {
     return (
         <>
+            {' '}
             <RegForm id={params.id} />
         </>
     );
